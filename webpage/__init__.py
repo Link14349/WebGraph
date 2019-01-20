166ca5ceecfbb4c804dde272e7d76ebd0ea245ba
yhzheng <32408362+qianduanXIAOHAOZI@users.noreply.github.com>
2019-01-20 18:43:08 +0800

    Added some comments of package webpage

--- webpage/__init__.py
+++ webpage/__init__.py
@@ -1,14 +1,21 @@
+"""
+How to use it is visible https://github.com/qianduanXIAOHAOZI/WebGraph/blob/master/README.md
+"""
+
 class webpage:
     def __init__(self, url, weight = 0.001, links = []):
-        self.__url = url
-        self.weight = weight
-        self.links = links
+        self.__url = url # you can't change it, but you can get it
+        self.weight = weight # you can change and get it, because the value of this needs to be recalculated frequently
+        self.links = links # you can change and get it, because the value of this also needs to add elements and get their values often.
 
-    def url(self):
+    def url(self): # This is the way to get url
         return self.__url
 
 
-def weight(links):
+# To calculat the weight according to links
+def weight(links): # links is of a webpage object
+    # The initial state is 0
+    # Then add the weight of all the pages that point to it
     w = 0
     for i in links:
         w += i.weight
